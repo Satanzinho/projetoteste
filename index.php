bf50ef7e349be6b0f95dec458b158a2f8de60253
Jorge <lassancejorgeluiz@gmail.com>
2019-04-20 22:19:30 -0300

    Alterando arquivo index.php expressando minha felicidade

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-Mudando um pouquinho
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ACHO QUE AGORA SIM FUNCIONA CARALHO</title>
+</head>
+<body>
+<H1>CREIO QUE VOU FICAR FELIZ SE ISSO FUNCIONAR</H1>
+</body>
+</html>
